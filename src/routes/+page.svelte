3eebb45102cbd556aa4037bc875bd5fd8faf908a
Max McKinney <maxgames101@gmail.com>
2024-03-01 18:13:18 -0600

    storage: move hero vid to blob

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     import Container from '$lib/components/Container.svelte';
     import HeroButton from '$lib/components/HeroButton.svelte';
-
-    import heroCauldron from '$lib/assets/videos/cauldron-hero-v3.mp4';
     import OverviewCard from '$lib/components/OverviewCard.svelte';
 </script>
 
@@ -16,7 +14,7 @@
                 <a class="bg-white border border-zinc-400 rounded-[13px] py-4 px-6 inline-flex justify-center items-center gap-3 font-medium text-xl leading-none cursor-default transition-all focus-visible:outline-offset-4" href="#">App Store - coming soon</a>
             </div>
             <video autoplay loop muted playsinline class="absolute -left-[70px] mt-6 sm:left-auto sm:-right-[150px] sm:top-[300px] md:top-[250px] lg:top-[200px] w-[780px] -z-10">
-                <source src={heroCauldron} type="video/mp4" />
+                <source src="https://yhdbff7qdnrdylls.public.blob.vercel-storage.com/cauldron-hero-v3-2JZx5SmfomoctzHXijfBtpsxAljBzC.mp4" type="video/mp4" />
             </video>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-[160px] sm:mt-[100px]">
                 <OverviewCard title="The only limit is your imagination" description="Powered by GPT-3.5 ML model trained with additional context, brewing a color palette has never been easier." type="imagination" />
