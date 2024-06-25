cfa46b57d40ea0c73cddd7ed03293eb6dfd62218
Max McKinney <max@maxmckinney.com>
2024-06-25 13:22:35 -0500

    ux: add notify affect to buttons when adding a fav palette

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+    import { animate, spring, timeline } from 'motion';
     import { createEventDispatcher } from 'svelte';
     export let text: string = '';
     export let icon: string = '';
     export let href: string = '';
+    export let id: string = '';
     export let layout: 'full' | 'icon' = 'full';
     export let type: 'default' | 'danger' = 'default';
+    const dispatch = createEventDispatcher();
+
+    let selfRef: HTMLElement;
+    let iconRef: any;
 
     const layoutClasses = {
         full: 'py-4 px-6',
@@ -16,22 +22,47 @@
         danger: 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700'
     };
 
-    const dispatch = createEventDispatcher();
+    export function notify() {
+        // Get the starting color of the button so that we can animate back to it
+        const startingColor = getComputedStyle(selfRef).backgroundColor;
+        timeline([
+            // Animate the button
+            [
+                selfRef,
+                {
+                    backgroundColor: [startingColor, '#6dffa2', startingColor],
+                    scale: [1, 1.05, 1]
+                },
+                { duration: 0.7 }
+            ],
+            // Animate the icon
+            [
+                iconRef,
+                {
+                    scale: [1, 1.3, 1]
+                },
+                { duration: 0.8, at: 0 }
+            ]
+        ]).finished.then(() => {
+            // Remove inline styles created by the animation so that the hover effect can work again
+            selfRef.removeAttribute('style');
+        });
+    }
 </script>
 
 {#if href}
-    <a class="{layoutClasses[layout]} {typeClasses[type]} inline-flex items-center justify-center gap-2 rounded-[13px] font-medium leading-none transition-all focus-visible:outline-offset-4 {$$restProps.class}" {href}>
+    <a class="{layoutClasses[layout]} {typeClasses[type]} inline-flex items-center justify-center gap-2 rounded-[13px] font-medium leading-none transition-all focus-visible:outline-offset-4 {$$restProps.class}" {id} {href} bind:this={selfRef}>
         {#if icon}
-            <i class={icon} />
+            <i class={icon} bind:this={iconRef} />
         {/if}
         {#if layout === 'full'}
             {text}
         {/if}
     </a>
 {:else}
-    <button class="{layoutClasses[layout]} {typeClasses[type]} inline-flex items-center justify-center gap-2 rounded-[13px] font-medium leading-none transition-all focus-visible:outline-offset-4 {$$restProps.class}" on:click={() => dispatch('click')}>
+    <button class="{layoutClasses[layout]} {typeClasses[type]} inline-flex items-center justify-center gap-2 rounded-[13px] font-medium leading-none transition-all focus-visible:outline-offset-4 {$$restProps.class}" {id} bind:this={selfRef} on:click={() => dispatch('click')}>
         {#if icon}
-            <i class={icon} />
+            <i class={icon} bind:this={iconRef} />
         {/if}
         {#if layout === 'full'}
             {text}
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,7 +1,19 @@
-<script>
+<script lang="ts">
     import Container from '$lib/components/Container.svelte';
     import logo from '$lib/assets/color-cauldron-logo-pot.svg';
     import Button from '$lib/components/Button.svelte';
+    import { favoritePalettes } from '$lib/stores';
+
+    let favButton: Button;
+
+    let previousPaletteCount = $favoritePalettes.palettes.length || 0;
+
+    $: {
+        if (previousPaletteCount < $favoritePalettes.palettes.length) {
+            favButton.notify();
+        }
+        previousPaletteCount = $favoritePalettes.palettes.length;
+    }
 </script>
 
 <!-- Primary top nav -->
@@ -21,9 +33,9 @@
 
 <!-- Mobile nav bar -->
 <!-- TODO: Make this it's own component -->
-<div class="pointer-events-none fixed bottom-0 left-0 right-0 z-10 bg-gradient-to-t from-white to-white/0 md:hidden">
+<div class="pointer-events-none fixed bottom-0 left-0 right-0 z-20 bg-gradient-to-t from-white to-white/0 md:hidden">
     <div class="flex w-full justify-end gap-4 px-6 py-4">
         <Button icon="fa-regular fa-cauldron" layout="icon" href="/brew" class="pointer-events-auto" />
-        <Button text="Favorites" icon="fa-regular fa-heart" href="/favorites" class="pointer-events-auto" />
+        <Button text="Favorites" icon="fa-regular fa-heart" href="/favorites" class="pointer-events-auto" bind:this={favButton} />
     </div>
 </div>
