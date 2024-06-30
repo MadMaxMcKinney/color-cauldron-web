841d286b6f4e1ec3d6a213f8f889e80a6ba04cc0
Max McKinney <max@maxmckinney.com>
2024-06-30 15:54:19 -0500

    page: add error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import Button from '$lib/components/Button.svelte';
+    import Container from '$lib/components/Container.svelte';
+</script>
+
+<Container>
+    <div class="mt-14 flex w-full flex-col items-center justify-center gap-9">
+        <div class="flex flex-col items-center gap-4">
+            <i class="fa-regular fa-spider-black-widow text-2xl"></i>
+            <h1 class="text-2xl font-medium">{$page.status}: {$page.error?.message}</h1>
+            <p class="opacity-60">We couldn't find what you are looking for, try again or return home.</p>
+        </div>
+        <Button href="/" text="Go Home" />
+    </div>
+</Container>
