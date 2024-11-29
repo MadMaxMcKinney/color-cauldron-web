4ea909a656009c7a2f9ecaaf26949401c2513ff3
Max McKinney <max@maxmckinney.com>
2024-11-28 22:30:30 -0600

    style: add icons to OverviewCard

--- src/lib/components/OverviewCard.svelte
+++ src/lib/components/OverviewCard.svelte
@@ -5,10 +5,9 @@
 
     export let title: string;
     export let description: string;
-
     export let type: 'quickly-save' | 'api' | 'imagination' = 'quickly-save';
-
     export let size: 'single' | 'double' = 'single';
+    export let icon: string | undefined = undefined;
 
     let sizeClasses = {
         single: 'col-span-1',
@@ -22,6 +21,11 @@
 </script>
 
 <div class="shadow-elevated bg-surface-primary/70 flex flex-col gap-4 rounded-[13px] border-2 border-white px-8 py-6 backdrop-blur-2xl lg:bg-[#FAFAFA]/60 {sizeClasses[size]}">
+    {#if icon}
+        <span class="bg-brand-green/15 text-brand-green grid aspect-square w-[40px] place-items-center rounded-xl text-xl">
+            <i class={icon} />
+        </span>
+    {/if}
     <h3 class="font-serif text-2xl font-bold text-black">{title}</h3>
     <p class="text-zinc-500 {descriptionTextClasses[size]}">{description}</p>
     <div class="flex flex-1 items-center justify-center">
