bcf8fa7f7fbbf484841373fc3ab29e1453caa690
Max McKinney <max@maxmckinney.com>
2024-11-28 21:34:28 -0600

    fix: tailwind config in css

--- src/lib/components/HeroButton.svelte
+++ src/lib/components/HeroButton.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <a
-    class="inline-flex h-14 items-center justify-center gap-3 rounded-[13px] border border-white bg-[#3FF381] px-6 text-xl leading-none font-medium shadow-xl shadow-green-300/75 transition-all hover:-translate-y-[2px] hover:bg-[#6dffa2] hover:shadow-2xl hover:shadow-green-300 focus-visible:outline-offset-4 {$$restProps.class}"
+    class="bg-brand-green inline-flex h-14 items-center justify-center gap-3 rounded-[13px] border border-white px-6 text-xl leading-none font-medium shadow-xl shadow-green-300/75 transition-all hover:-translate-y-[2px] hover:bg-[#6dffa2] hover:shadow-2xl hover:shadow-green-300 focus-visible:outline-offset-4 {$$restProps.class}"
     {href}
 >
     <i class={icon} />
