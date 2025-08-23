b1090d132b596fee32afee1b449ac29c9b7250d5
Max McKinney <max@maxmckinney.com>
2025-08-22 20:11:33 -0500

    design: remove arrow from footer logo

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,9 +6,7 @@
 <footer class="animate-fade-in-slower mt-16 py-8">
     <Container>
         <div class="flex items-center justify-between gap-8">
-            <a href="https://hellishcreative.com" class="flex items-center gap-2 text-sm font-bold text-black opacity-100 transition-all hover:opacity-60 sm:text-base"
-                ><img class="h-[32px]" src={HellishCreativeLogo} alt="Hellish Creative Logo" /><i class="fa-regular fa-arrow-up-right-from-square text-sm text-zinc-500" /></a
-            >
+            <a href="https://hellishcreative.com" class="flex items-center gap-2 text-sm font-bold text-black opacity-100 transition-all hover:opacity-60 sm:text-base"><img class="h-[32px]" src={HellishCreativeLogo} alt="Hellish Creative Logo" /></a>
         </div>
     </Container>
 </footer>
